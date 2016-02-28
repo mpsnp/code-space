8cd58beaffda41c64ea8ee64110caa2f1d5fd753
George Kiriy <gkiriy@smedialink.com>
2016-02-28 14:47:09 +0300

    Irrlicht try. In order to simplify rendering.

--- main.cpp
+++ main.cpp
@@ -1,79 +1,33 @@
-#include <GLFW/glfw3.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string>
-#include <tclap/CmdLine.h>
+#include <irrlicht.h>
 
-static void error_callback(int error, const char *description) {
-    fputs(description, stderr);
-}
-
-static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
-}
+using namespace irr;
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
 
 int main(int argc, char **argv) {
+    const dimension2d<u32> &windowSize = core::dimension2d<u32>(640, 480);
+    int bits = 16;
+    bool fullscreen = false;
+    bool stencilbuffer = false;
+    bool vsync = false;
+    IrrlichtDevice *device = createDevice(EDT_OPENGL, windowSize, bits, fullscreen, stencilbuffer, vsync);
+    if (device == 0) return 1;
 
-    TCLAP::CmdLine cmd("Space Code --- source code visualisation tool", ' ', "0.1");
-    TCLAP::SwitchArg fullScreenArg("f", "fullscreen", "Open in full screen window", cmd);
-    TCLAP::SwitchArg autosizeArg("a", "autosize", "Use current resolution", cmd);
-    TCLAP::ValueArg<int> widthArg("W", "width", "Width of viewport", false, 640, "int", cmd);
-    TCLAP::ValueArg<int> heightArg("H", "height", "Height of viewport", false, 480, "int", cmd);
-    cmd.parse(argc, (const char *const *) argv);
+    IVideoDriver *driver = device->getVideoDriver();
+    ISceneManager *smgr = device->getSceneManager();
 
-    // Init GLFW
-    GLFWwindow *window;
-    glfwSetErrorCallback(error_callback);
-    if (!glfwInit()) {
-        exit(EXIT_FAILURE);
-    }
+    while (device->run())
+        if (device->isWindowActive()) {
 
-    // Create window
-    GLFWmonitor *monitor = glfwGetPrimaryMonitor();
-    bool fullScreen = fullScreenArg.getValue();
-    int width = widthArg.getValue();
-    int height = heightArg.getValue();
-    if (autosizeArg.getValue()) {
-        const GLFWvidmode *mode = glfwGetVideoMode(monitor);
-        width = mode->width;
-        height = mode->height;
-    }
-    window = glfwCreateWindow(width, height, "Simple example", fullScreen ? monitor : NULL, NULL);
-    if (!window) {
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
+            driver->beginScene(true, true, SColor(0, 3, 243, 250));
+            smgr->drawAll();
+            driver->endScene();
+        }
 
-    // Start drawing loop
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(1);
-    glfwSetKeyCallback(window, key_callback);
-    while (!glfwWindowShouldClose(window)) {
-        float ratio;
-        int width, height;
-        glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / (float) height;
-        glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT);
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.f, 0.6f, 0.f);
-        glEnd();
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    exit(EXIT_SUCCESS);
+    device->drop();
+
+    return 0;
 }
