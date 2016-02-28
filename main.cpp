516b3e72baa3a739c5067a4ea993e8d991e17ace
George Kiriy <gkiriy@smedialink.com>
2016-02-28 15:43:56 +0300

    CL args

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <irrlicht.h>
+#include <tclap/CmdLine.h>
+
 
 using namespace irr;
 using namespace core;
@@ -8,11 +10,18 @@ using namespace io;
 using namespace gui;
 
 int main(int argc, char **argv) {
-    const dimension2d<u32> &windowSize = core::dimension2d<u32>(640, 480);
-    int bits = 16;
-    bool fullscreen = false;
+    TCLAP::CmdLine cmd("Space Code --- source code visualisation tool", ' ', "0.1");
+    TCLAP::SwitchArg fullScreenArg("f", "fullscreen", "Open in full screen window", cmd);
+    TCLAP::SwitchArg vsyncArg("v", "vsync", "Enable vertical synchronisation", cmd);
+    TCLAP::ValueArg<u32> widthArg("W", "width", "Width of viewport", false, 640, "int", cmd);
+    TCLAP::ValueArg<u32> heightArg("H", "height", "Height of viewport", false, 480, "int", cmd);
+    cmd.parse(argc, (const char *const *) argv);
+
+    const dimension2d<u32> &windowSize = core::dimension2d<u32>(widthArg.getValue(), heightArg.getValue());
+    u32 bits = 16;
+    bool fullscreen = fullScreenArg.getValue();
     bool stencilbuffer = false;
-    bool vsync = false;
+    bool vsync = vsyncArg.getValue();
     IrrlichtDevice *device = createDevice(EDT_OPENGL, windowSize, bits, fullscreen, stencilbuffer, vsync);
     if (device == 0) return 1;
 
@@ -22,7 +31,7 @@ int main(int argc, char **argv) {
     while (device->run())
         if (device->isWindowActive()) {
 
-            driver->beginScene(true, true, SColor(0, 3, 243, 250));
+            driver->beginScene(true, true, SColor(0, 0, 0, 0));
             smgr->drawAll();
             driver->endScene();
         }
